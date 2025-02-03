afde7bd172cd22cf1ed62822f84cd584eab22f4f
Matheus Sampaio <matheuslemos98@gmail.com>
2025-02-03 16:36:17 -0300

    rodando com shared preferences

--- fuel_tune/lib/screens/home/settings_page.dart
+++ fuel_tune/lib/screens/home/settings_page.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -13,9 +15,41 @@ class _SettingsPageState extends State<SettingsPage> {
   @override
   void initState() {
     super.initState();
+    _calcularTamanhoSharedPreferences();
   }
 
+  Future<void> _calcularTamanhoSharedPreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+    int totalSize = 0;
 
+    prefs.getKeys().forEach((key) {
+      final value = prefs.get(key);
+      if (value != null) {
+        totalSize += utf8.encode(value.toString()).length;
+      }
+    });
+
+    setState(() {
+      _sizeMessage = "Uso de armazenamento: ${_formatarTamanho(totalSize)}";
+    });
+  }
+
+  String _formatarTamanho(int bytes) {
+    if (bytes < 1024) return "$bytes B";
+    if (bytes < 1024 * 1024) return "${(bytes / 1024).toStringAsFixed(2)} KB";
+    return "${(bytes / (1024 * 1024)).toStringAsFixed(2)} MB";
+  }
+
+  Future<void> _limparSharedPreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
+    setState(() {
+      _sizeMessage = "Uso de armazenamento: 0 B";
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Dados salvos foram limpos com sucesso.')),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,16 +65,14 @@ class _SettingsPageState extends State<SettingsPage> {
             ListTile(
               title: const Text('Limpar dados salvos'),
               leading: const Icon(Icons.delete, color: Colors.red),
-              onTap: () {
-              },
+              onTap: _limparSharedPreferences,
             ),
             const Divider(),
             Padding(
               padding: const EdgeInsets.only(top: 16.0),
               child: Text(
                 _sizeMessage,
-                style:
-                    const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
             ),
           ],
